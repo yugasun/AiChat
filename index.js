0e1b12bd15e08f2ca0b76e615b14dc3c41d2071b
yugasun <yuga_sun@163.com>
2017-08-24 17:07:03 +0800

    Add https

--- index.js
+++ index.js
@@ -1,34 +1,53 @@
-const express = require('express')
-const app = express()
+const path = require('path')
+const http = require('http')
+const https = require('https')
+const fs = require('fs')
 
+const Koa = require('koa')
+const KoaRouter = require('koa-router')
+const KoaStatic = require('koa-static')
+const KoaSslify = require('koa-sslify')
+const IO = require('koa-socket')
 const SendChat = require('./tuling-ai')
 
-// html
-app.use(express.static(__dirname + '/views'))
-// js, css, images
-app.use(express.static(__dirname + '/public'))
+const app = new Koa()
+const router = new KoaRouter()
+const io = new IO()
 
-const server = app.listen(5000)
-
-const io = require('socket.io')(server)
-
-// Web UI
-app.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   res.sendFile('index.html')
 })
 
+// Force HTTPS on all page
+app.use(KoaSslify({
+  trustProtoHeader: true
+}))
+
+// html
+app.use(KoaStatic(path.join(__dirname, 'views')))
+// js, css, images
+app.use(KoaStatic(path.join(__dirname, 'public')))
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+io.attach(app)
+
 io.on('connection', (socket) => {
   console.log('a user connected')
-
-  socket.on('chat message', (text) => {
-    // get a replay from baidu ai
-    SendChat(text)
-      .then((res) => {
-        console.log(res.results[0].values.text)
-        socket.emit('bot reply', res.results[0].values.text)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  })
 })
+
+io.on('chat message', (socket) => {
+  const text = socket.data
+
+  SendChat(text)
+    .then((res) => {
+      console.log(res.results[0].values.text)
+      io.broadcast('bot reply', res.results[0].values.text)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+})
+
+app.listen(5000)
